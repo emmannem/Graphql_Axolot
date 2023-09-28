641ce091c5b4ec0a1084c308a16cbb4e2061caa0
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2023-09-28 15:56:11 -0600

    add mutation NewUser and config #2

---
+++ api_graphql_Axo/schema.py
@@ -0,0 +1,17 @@
+import graphene
+
+#import links.schema
+import users.schema
+
+class Query(
+    users.schema.Query,
+    graphene.ObjectType,
+):
+    pass
+
+
+class Mutation(users.schema.Mutation, graphene.ObjectType,):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
